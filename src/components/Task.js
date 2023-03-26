d26e494ab8436b0e4420734df850dd0c04658159
d1bolozan <d1bolozan@gmail.com>
2023-03-26 13:22:44 +0300

    first UI component

---
+++ src/components/Task.js
@@ -0,0 +1,34 @@
+const Task = ({ task: { id, title, state }, onArchiveTask, onPinTask }) => {
+	return (
+		<div className={`list-item ${state}`}>
+			<label htmlFor="checked" aria-label={`archiveTask-${id}`} className="checkbox">
+				<input
+					type="checkbox"
+					name="checked"
+					id={`archiveTask-${id}`}
+					disabled={true}
+					checked={state === "TASK_ARCHIVED"}
+				/>
+				<span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
+			</label>
+
+			<label htmlFor="title" aria-label={title} className="title">
+				<input type="text" value={title} name="title" readOnly={true} placeholder="Input title" />
+			</label>
+
+			{state !== "TASK_ARCHIVED" && (
+				<button
+					className="pin-button"
+					onClick={() => onPinTask(id)}
+					id={`pinTask-${id}`}
+					aria-label={`pinTask-${id}`}
+					key={`pinTask-${id}`}
+				>
+					<span className={`icon-star`} />
+				</button>
+			)}
+		</div>
+	);
+};
+
+export default Task;
