6b4c06954e526b810c1dc84dd133aa560930abf5
d1bolozan <d1bolozan@gmail.com>
2023-03-26 15:06:43 +0300

    redux with stories

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,30 @@
+import { useDispatch, useSelector } from "react-redux";
+import { updateTaskState } from "../lib/store";
 import Task from "./Task";
 
-const TaskList = ({ loading, tasks, onPinTask, onArchiveTask }) => {
-	const events = {
-		onPinTask,
-		onArchiveTask,
+const TaskList = () => {
+	const tasks = useSelector(state => {
+		const tasksInOrder = [
+			...state.taskbox.tasks.filter(t => t.state === "TASK_PINNED"),
+			...state.taskbox.tasks.filter(t => t.state !== "TASK_PINNED"),
+		];
+		const filteredTasks = tasksInOrder.filter(
+			t => t.state === "TASK_INBOX" || t.state === "TASK_PINNED"
+		);
+
+		return filteredTasks;
+	});
+
+	const { status } = useSelector(state => state.taskbox);
+
+	const dispatch = useDispatch();
+
+	const pinTask = value => {
+		dispatch(updateTaskState({ id: value, newTaskState: "TASK_PINNED" }));
+	};
+
+	const archiveTask = value => {
+		dispatch(updateTaskState({ id: value, newTaskState: "TASK_ARCHIVED" }));
 	};
 
 	const LoadingRow = (
@@ -17,7 +38,7 @@ const TaskList = ({ loading, tasks, onPinTask, onArchiveTask }) => {
 		</div>
 	);
 
-	if (loading) {
+	if (status === "loading") {
 		return (
 			<div className="list-items" data-testid="loading" key={`loading`}>
 				{LoadingRow}
@@ -42,15 +63,15 @@ const TaskList = ({ loading, tasks, onPinTask, onArchiveTask }) => {
 		);
 	}
 
-	const tasksInOrder = [
-		...tasks.filter(t => t.state === "TASK_PINNED"),
-		...tasks.filter(t => t.state !== "TASK_PINNED"),
-	];
-
 	return (
 		<div className="list-items">
-			{tasksInOrder.map(task => (
-				<Task key={task.id} task={task} {...events} />
+			{tasks.map(task => (
+				<Task
+					key={task.id}
+					task={task}
+					onPinTask={task => pinTask(task)}
+					onArchiveTask={task => archiveTask(task)}
+				/>
 			))}
 		</div>
 	);
